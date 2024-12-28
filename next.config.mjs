8c816f1d9c3f8a07d0f347737b562a8f9fd67f65
Marcelo Lanegra <marcelo3096@gmail.com>
2024-12-28 11:49:45 -0500

    feat: added image upload component

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-    ]
-  }
-};
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
